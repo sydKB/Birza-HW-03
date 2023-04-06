df3d5b32d63da62ae4d1f644dfbc34b1d28008a7
sydKB <sydneykb11@icloud.com>
2023-04-06 15:21:36 -0700

    JS and comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,49 @@
- // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Ask user for length (Minimum of 8 characters, maximum of 128)
+  // Ask user for password length (Minimum of 8 characters, maximum of 128)
   let passwordLength = parseInt(prompt("How long should the password be?"));
   
-  // Validate length
+  // Check password length, alert if out of specified bounds
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Error, invalid password length. \nPlease choose a password length greater than 8 and less than 128 characters.");
     return "";
   }
 
-  // Ask for which characters to include
+  // Ask for which character types to include using confirmation bubbles
   var includeLowercase = confirm("Include lowercase letters in password?");
   var includeUppercase = confirm("Include uppercase letters in password?");
   var includeNumbers = confirm("Include numbers in password?");
   var includeSpecialCharacters = confirm("Include special characters in password?");
   
-  // Validate types of characters
-  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialCharacters)
-  {
+  // Error message if no character types selected. 
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialCharacters) {
     alert("Error, insufficient character types. \nPlease include at least one type of characte");
     return "";
   } 
 
-  // Generate random password & return
+  // Defining character types as const strings
   let passwordCharacters = [];
-  const specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  const specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberCharacters = "0123456789";
   
-  // TODO handle other character types
-  
+  // Concatenating, if confirmed, characters from aforementioned strings to array, passwordCharacters
   if (includeSpecialCharacters) {
     passwordCharacters = passwordCharacters.concat(specialCharacters.split(""));
   }
+  if (includeLowercase) {
+    passwordCharacters = passwordCharacters.concat(lowercaseCharacters.split(""));
+  }
+  if (includeUppercase) {
+    passwordCharacters = passwordCharacters.concat(uppercaseCharacters.split(""));
+  }
+  if (includeNumbers) {
+    passwordCharacters = passwordCharacters.concat(numberCharacters.split(""));
+  }
+
+  // Adding random characters from confirmed character type array to password result
   let results = "";
   for (var i = 0; i < passwordLength; i++) {
     let randomIndex = Math.floor(Math.random() * passwordCharacters.length);
@@ -40,10 +51,8 @@ function generatePassword() {
     results += randomCharacter;    
   }
 
-
-
-
-
+  // Return generated password
+  return results;
 }
 
 // Write password to the #password input
@@ -52,7 +61,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
